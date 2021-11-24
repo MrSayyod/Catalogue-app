0dda9a40e53e0f874f69dc99aedce6e44623d5ec
MrSayyod <ssayyod@gmail.com>
2021-11-24 17:22:43 -0600

    Create ProductComponent component

---
+++ src/components/ProductComponent.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ProductComponent = () => {
+  return (
+    <div className="menu_pdcmp">
+      <div className="container_pdcmp">
+        <h2>Heey ProductComponent</h2>
+      </div>
+    </div>
+  );
+};
+
+export default ProductComponent;
