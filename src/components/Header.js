5e8d524ad8c30b4cb8a5a3370496970d916c32ee
MrSayyod <ssayyod@gmail.com>
2021-11-24 17:17:33 -0600

    Create Header component

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="menu">
+      <div className="container">
+        <h2>Heey header</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
