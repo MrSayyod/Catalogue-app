2eb655ee1eb57a2e94592ca392694fee4012a3b6
MrSayyod <ssayyod@gmail.com>
2021-11-24 16:54:53 -0600

    Add Redux devtool extension

--- src/store.js
+++ src/store.js
@@ -1,5 +1,9 @@
 import { createStore } from "redux";
 import { reducers } from "./reducers/index";
 
-const store = createStore(reducers, {})
-export default store
+const store = createStore(
+  reducers,
+  {},
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+export default store;
