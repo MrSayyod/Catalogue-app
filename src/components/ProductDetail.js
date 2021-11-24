9eba24dee1ba1ba3ce5c52e8b0d89f3863fe1c4b
MrSayyod <ssayyod@gmail.com>
2021-11-24 17:24:24 -0600

    Create ProductDetail component

---
+++ src/components/ProductDetail.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ProductDetail = () => {
+  return (
+    <div className="menu_pddtl">
+      <div className="container_pddtl">
+        <h2>Heey ProductDetail</h2>
+      </div>
+    </div>
+  );
+};
+
+export default ProductDetail;
