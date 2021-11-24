743735d25a8a44cb8661e01020f452f4f4454bd9
MrSayyod <ssayyod@gmail.com>
2021-11-24 17:21:39 -0600

    Create ProductListing component

---
+++ src/components/ProductListing.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ProductListing = () => {
+  return (
+    <div className="menu_pdls">
+      <div className="container_pdls">
+        <h2>Heey ProductListing</h2>
+      </div>
+    </div>
+  );
+};
+
+export default ProductListing;
