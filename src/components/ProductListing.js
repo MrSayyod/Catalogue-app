5b7bda36c5639fda2779e9e0b8deabc372b78a52
MrSayyod <ssayyod@gmail.com>
2021-11-25 16:49:56 -0700

    Import and use useDispatch hook

--- src/components/ProductListing.js
+++ src/components/ProductListing.js
@@ -2,16 +2,19 @@ import React, { useEffect } from "react";
 import axios from "axios";
 import { useSelector } from "react-redux";
 import ProductComponent from "./ProductComponent";
+import { useDispatch } from "react-redux";
 
 const ProductListing = () => {
   const products = useSelector((state) => state);
+  const dispatch = useDispatch()
   const fetchProducts = async () => {
     const response = await axios
       .get("https://fakestoreapi.com/products")
       .catch((error) => {
         console.log("Error", error);
       });
-    console.log(response.data);
+      // console.log(response.data);
+      dispatch(response.data)
   };
   useEffect(() => {
     fetchProducts();
