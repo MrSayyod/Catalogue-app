905b386483f47da285cc8256e6a6ba692e663fd8
MrSayyod <ssayyod@gmail.com>
2021-11-25 16:24:13 -0700

    Add useSelector to access Redux store

--- src/components/ProductListing.js
+++ src/components/ProductListing.js
@@ -1,6 +1,9 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const ProductListing = () => {
+  const products = useSelector((state) => state)
+  console.log(products);
   return (
     <div className="menu_pdls">
       <div className="container_pdls">
