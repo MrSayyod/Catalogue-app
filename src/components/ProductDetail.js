c2274ed7e57bc42f1ca8f29bca4679f3314fee5f
MrSayyod <ssayyod@gmail.com>
2021-11-25 18:11:28 -0700

    Import removeSelectedProduct action

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
-import { selectedProduct } from "../actions/productActions";
+import { selectedProduct, removeSelectedProduct } from "../actions/productActions";
 
 const ProductDetail = () => {
   const product = useSelector((state) => state.product);
