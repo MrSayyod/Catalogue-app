af9731a206ec7e9959dc4a00638a45aed4ac081f
MrSayyod <ssayyod@gmail.com>
2021-11-25 17:29:23 -0700

    Import useEffect, useParams hooks and axios for API

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, {useEffect} from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
 const ProductDetail = () => {
   return (
