064343e588cdbb0b21f0e66b54541e7df4675665
MrSayyod <ssayyod@gmail.com>
2021-11-25 17:17:38 -0700

    Render all product list

--- src/components/ProductComponent.js
+++ src/components/ProductComponent.js
@@ -3,20 +3,27 @@ import { useSelector } from "react-redux";
 
 const ProductComponent = () => {
   const products = useSelector((state) => state.allProducts.products);
-  // const { id, title } = products[0];
-  return (
-    <div className="menu_pdcmp">
-      <div className="container_pdcmp">
-        <div className="card">
-          <div className="image"></div>
-          <div className="content">
-            {/* <div className="header">{title}</div> */}
+  const renderList = products.map((product) => {
+    const { id, title, image, price, category } = product;
+    return (
+      <div className="menu_pdcmp" key={id}>
+        <div className="container_pdcmp">
+          <div className="card">
+            <div className="image">
+              <img src={image} alt={title} />
+            </div>
+            <div className="content">
+              <div className="header">{title}</div>
+              <div className="price">$ {price}</div>
+              <div className="category">{category}</div>
+            </div>
           </div>
+          {/* <h2>Heey ProductComponent</h2> */}
         </div>
-        {/* <h2>Heey ProductComponent</h2> */}
       </div>
-    </div>
-  );
+    );
+  });
+  return <>{renderList}</>;
 };
 
 export default ProductComponent;
