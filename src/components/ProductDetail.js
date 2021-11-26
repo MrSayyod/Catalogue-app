ee9d1d38300194a8a363526d5adfc4fd4ee32c91
MrSayyod <ssayyod@gmail.com>
2021-11-25 18:06:35 -0700

    Render product details

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -6,6 +6,7 @@ import { selectedProduct } from "../actions/productActions";
 
 const ProductDetail = () => {
   const product = useSelector((state) => state.product);
+  const { title, image, price, category, description } = product
   const { productId } = useParams();
   const dispatch = useDispatch();
   console.log(product);
@@ -25,9 +26,43 @@ const ProductDetail = () => {
     }
   }, [productId]);
   return (
-    <div className="menu_pddtl">
-      <div className="container_pddtl">{/* <h2>Heey ProductDetail</h2> */}</div>
+    <div className="container">
+      {Object.keys(product).length === 0 ? (
+        <div>...Loading</div>
+      ) : (
+        <div className="placeholder segment">
+          <div className="two column center grid"> 
+            <div className="vertical divider">AND</div>
+            <div className="middle aligned row">
+              <div className='column lp'>
+                <img className="fluid image" src={image} />
+              </div>
+              <div className='column rp'>
+                <h1>{title}</h1>
+                <h2>
+                  <a className="teal tag label">$ {price}</a>
+                </h2>
+                <h3 className="brown block header">{category}</h3>
+                <p>{description}</p>
+                <div className="vertical animated button" tabaIndex="0">
+                  <div className="hidden content">
+                    <i className="shop icon"></i>
+                  </div>
+                  <div className="visible content">Add to Cart</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
+
+    // <div className="menu_pddtl">
+    //   <div className="container_pddtl">
+        
+    //     {/* <h2>Heey ProductDetail</h2> */}
+    //   </div>
+    // </div>
   );
 };
 
