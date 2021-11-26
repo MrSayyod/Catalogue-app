e7b15c1568344dada9dc7a73a8b1d17eb51fc176
MrSayyod <ssayyod@gmail.com>
2021-11-25 17:36:33 -0700

    Use dispatch to selected product action

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -1,13 +1,27 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { selectedProduct } from "../actions/productActions";
 
 const ProductDetail = () => {
+  const product = useSelector((state)=> state.product)
+  const { productId } = useParams();
+  const dispatch = useDispatch()
+  console.log(productId);
+
+  const fetchProductDetail = async() => {
+    const response = await axios
+      .get(`https://fakestoreapi.com/products/${productId}`)
+      .catch((error) => {
+        console.log("Error", error);
+      });
+
+      dispatch(selectedProduct(response.data))
+  };
   return (
     <div className="menu_pddtl">
-      <div className="container_pddtl">
-        <h2>Heey ProductDetail</h2>
-      </div>
+      <div className="container_pddtl">{/* <h2>Heey ProductDetail</h2> */}</div>
     </div>
   );
 };
