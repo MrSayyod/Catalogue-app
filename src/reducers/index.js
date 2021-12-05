372ddea93229f3344bb5f3dcdbced92ff60bcec9
MrSayyod <ssayyod@gmail.com>
2021-12-04 17:32:04 -0700

    Add filter reducer into combiner

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
+import filterReducer from "./filter";
 import { productReducer, selectedProductReducer } from "./productReducer";
 
 const reducers = combineReducers({
   allProducts: productReducer,
   product: selectedProductReducer,
+  filter: filterReducer,
 })
 
 export default reducers
