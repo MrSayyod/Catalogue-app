342ab1bc32717ff7f87b627e20e6ffcc9d674dd6
MrSayyod <ssayyod@gmail.com>
2021-12-04 18:20:02 -0700

    Create Categoryfilter.js and create categories to filter

---
+++ src/components/CategoryFilter.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { classDeclaration } from '@babel/types'
+
+const CATEGORIES = [
+  "men's clothing",
+  "jewelery",
+  "electronics",
+  "women's clothing"
+]
+
