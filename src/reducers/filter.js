7002de15c700693105446cb8c40f304d5b7d9b80
MrSayyod <ssayyod@gmail.com>
2021-12-04 17:29:04 -0700

    Create filter reducer

---
+++ src/reducers/filter.js
@@ -0,0 +1,12 @@
+import { ActionTypes } from "../containers/action-types";
+
+const filterReducer = (state = "ALL", action) => {
+  switch (action.type) {
+    case ActionTypes.CHANGE_FILTER:
+      return action.filter;
+    default:
+      return state;
+  }
+};
+
+export default filterReducer
