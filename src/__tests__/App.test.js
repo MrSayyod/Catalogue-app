2a3779d310b9d215569f7781b407362ea88444ab
MrSayyod <ssayyod@gmail.com>
2021-12-10 20:38:21 +0500

    Import necessary files and libraries

---
+++ src/__tests__/App.test.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import App from '../App'
+import { act } from 'react-dom/test-utils'
+import { render, unmountComponentAtNode } from 'react-dom'
+import pretty from 'pretty'
+
