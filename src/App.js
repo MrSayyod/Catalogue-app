fe4f7da48f800715dcce11ae625e29cf0f1e627d
MrSayyod <ssayyod@gmail.com>
2021-12-10 21:04:34 +0500

    Write test for App.js and fix dependencies

--- src/App.js
+++ src/App.js
@@ -3,18 +3,22 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./components/Header";
 import ProductListing from "./components/ProductListing";
 import ProductDetail from "./components/ProductDetail";
+import { Provider } from "react-redux";
+import store from "./store";
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Header />
-        <Switch>
-          <Route path="/" exact component={ProductListing} />
-          <Route path="/product/:productId" exact component={ProductDetail} />
-          <Route>404 Not Found!</Route>
-        </Switch>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <Header />
+          <Switch>
+            <Route path="/" exact component={ProductListing} />
+            <Route path="/product/:productId" exact component={ProductDetail} />
+            <Route>404 Not Found!</Route>
+          </Switch>
+        </Router>
+      </Provider>
     </div>
   );
 }
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,6 +1,24 @@
-import React from 'react'
-import App from '../App'
-import { act } from 'react-dom/test-utils'
-import { render, unmountComponentAtNode } from 'react-dom'
-import pretty from 'pretty'
+import React from "react";
+import App from "../App";
+import { act } from "react-dom/test-utils";
+import { render, unmountComponentAtNode } from "react-dom";
+import pretty from "pretty";
 
+let container = null;
+beforeEach(() => {
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+it("renders correctly", () => {
+  act(() => {
+    render(<App />, container);
+  });
+  expect(pretty(container.innerHTML)).toMatchSnapshot();
+});
