dcdde244eb8575b79a9b5fcda595dee950512a25
MrSayyod <ssayyod@gmail.com>
2021-12-19 18:24:26 -0700

    Add bootstrap claasses to Product component

--- src/components/ProductComponent.js
+++ src/components/ProductComponent.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import '../stylesheets/ProductComponent.css'
 
 const ProductComponent = ({ items }) => {
   const renderList = items.map(product => {
@@ -8,20 +9,20 @@ const ProductComponent = ({ items }) => {
       id, title, image, price, category,
     } = product;
     return (
-      <div className="menu_pdcmp" key={id}>
+      <div className="col-3" key={id}>
         <Link to={`/product/${id}`}>
           <div className="container_pdcmp">
-            <div className="card">
-              <div className="image">
-                <img src={image} alt={title} />
+            <div className="card p-2">
+              <div className="product_image">
+                <img src={image} alt={title} className="w-100 h-100" />
               </div>
-              <div className="content">
-                <div className="header">{title}</div>
-                <div className="price">
+              <div className="content d-flex flex-column justify-content-between pb-0">
+                <div className="product_header fw-bold text-decoration-none fs-6">{title}</div>
+                <div className="price fw-bold fs-6">
                   $
                   {price}
                 </div>
-                <div className="category">{category}</div>
+                <div className="category p-3 text-center rounded-3">{category}</div>
               </div>
             </div>
           </div>
