1fab3439912cb3c7972b063a3b6b625326a149e8
MrSayyod <ssayyod@gmail.com>
2021-12-23 12:47:35 -0700

    Add Router to fix test error

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
 
 const Header = () => (
   <div className="menu">
     <div className="container">
-      <Link to="/">
-        <h2 data-testid="header-text">Welcome to our store</h2>
-      </Link>
+      <Router>
+        <Link to="/">
+          <h2 data-testid="header-text">Welcome to our store</h2>
+        </Link>
+      </Router>
     </div>
   </div>
 );
