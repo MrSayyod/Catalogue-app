149e6a307ad476285369392e662a015b2c4867f9
MrSayyod <ssayyod@gmail.com>
2021-12-23 13:11:04 -0700

    Fix jest testing errors

--- src/__tests__/Header.test.js
+++ src/__tests__/Header.test.js
@@ -4,6 +4,7 @@ import { act } from 'react-dom/test-utils';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import { unmountComponentAtNode } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import Header from '../components/Header';
 
 let container = null;
@@ -20,12 +21,21 @@ afterEach(() => {
 
 it('renders correctly', () => {
   act(() => {
-    render(<Header />, container);
+    render(
+      <BrowserRouter>
+        <Header />
+      </BrowserRouter>,
+      container,
+    );
   });
   expect(pretty(container.innerHTML)).toMatchSnapshot();
 });
 
 it('should display the header text', () => {
-  const { getByTestId } = render(<Header />);
+  const { getByTestId } = render(
+    <BrowserRouter>
+      <Header />
+    </BrowserRouter>,
+  );
   expect(getByTestId('header-text')).toHaveTextContent('Welcome to our store');
 });
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Header = () => (
   <div className="menu">
     <div className="container">
-      <Router>
-        <Link to="/">
-          <h2 data-testid="header-text">Welcome to our store</h2>
-        </Link>
-      </Router>
+      <Link to="/">
+        <h2 data-testid="header-text">Welcome to our store</h2>
+      </Link>
     </div>
   </div>
 );
